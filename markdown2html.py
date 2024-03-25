215cdd06d4126b54abed488a5d9f5da9d49180e5
Cassyeh <cassandraijezie@gmail.com>
2024-03-25 00:18:22 +0000

    Modifications

--- markdown2html.py
+++ markdown2html.py
@@ -11,54 +11,139 @@ print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0
 """
 
-import argparse
-import pathlib
-import re
-import sys
 
+if __name__ == "__main__":
+    import sys
+    from os import path
+    import re
+    import hashlib
 
-def md_to_html(file_input, file_output):
-    '''
-    Converts markdown file to HTML file
-    '''
-    with open(file_input, encoding='utf-8') as f:
-        md_content = f.readlines()
+    hash_dict = {"#": "h1", "##": "h2", "###": "h3", "####": "h4",
+                 "#####": "h5", "######": "h6", "-": "ul", "*": "ol"}
 
-    html_content = []
-    for line in md_content:
-        # Check if the line is a heading
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            # Get the level of the heading
-            h_level = len(match.group(1))
-            # Get the content of the heading
-            h_content = match.group(2)
-            # Append the HTML equivalent of the heading
-            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
-        else:
-            html_content.append(line)
-
-    # Write the HTML content to the output file
-    with open(file_output, 'w', encoding='utf-8') as f:
-        f.writelines(html_content)
-
-
-if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
-    parser.add_argument('file_input', help='path to input markdown file')
-    parser.add_argument('file_output', help='path to output HTML file')
-    args = parser.parse_args()
 
-    # Check if the input file exists
-    input_path = pathlib.Path(args.file_input)
-    if not input_path.is_file():
-        print(f'Missing {input_path}', file=sys.stderr)
-        sys.exit(1)
+    if not path.exists(sys.argv[1]) or not str(sys.argv[1]).endswith(".md"):
+        sys.stderr.write("Missing " + sys.argv[1] + '\n')
+        exit(1)
 
-    # Convert the markdown file to HTML
-    md_to_html(args.file_input, args.file_output)
+    def funcHeadings(pattern):
+        tag = hash_dict[lineSplit[0]]
+        toWrite = line.replace("{} ".format(lineSplit[0]), "<{}>".format(tag))
+        toWrite = toWrite[:-1] + ("</{}>\n".format(tag))
+        fw.write(toWrite)
+
+    def funcInline(line, pattern):
+        flag = 0
+        while pattern in line:
+            if not flag:
+                if pattern == "**":
+                    line = line.replace(pattern, "<b>", 1)
+                    flag = 1
+                else:
+                    line = line.replace(pattern, "<em>", 1)
+                    flag = 1
+            else:
+                if pattern == "**":
+                    line = line.replace(pattern, "</b>", 1)
+                    flag = 0
+                else:
+                    line = line.replace(pattern, "</em>", 1)
+                    flag = 0
+        return line
+
+    def md5Markdown(line):
+        rep = []
+        while "[[" in line and "]]" in line:
+            rep = []
+            for count in range(len(line)):
+                if not count == len(line) - 1 and line[count] == '[' and line[count + 1] == '[':
+                    rep.append(count)
+                elif not count == len(line) - 1 and line[count] == "]" and line[count + 1] == ']':
+                    rep.append(count)
+            if rep:
+                sliceObj = slice(rep[0], rep[1] + 2)
+
+            toRep = line[sliceObj]
+            toHash = toRep[2:-2]
+            md = hashlib.md5(toHash.encode()).hexdigest()
+            line = line.replace(toRep, md)
+        return line
+
+    def funcForCases(line):
+        rep = []
+        s = ''
+        while '((' in line:
+            rep = []
+            for count in range(len(line)):
+                if not count == len(line) - 1 and line[count] == '(' and line[count + 1] == '(':
+                    rep.append(count)
+                elif not count == len(line) - 1 and line[count] == ")" and line[count + 1] == ')':
+                    rep.append(count)
+            if rep:
+                sliceObj = slice(rep[0], rep[1] + 2)
+            toRep = line[sliceObj]
+            s = toRep
+            for char in toRep:
+                if char == 'c':
+                    toRep = toRep.replace('c', '')
+                elif char == 'C':
+                    toRep = toRep.replace('C', '')
+            line = line.replace(s, toRep[2:-2])
+        return line
+
+    with open(sys.argv[1], mode='r') as fr, open(sys.argv[2], mode='w+') as fw:
+        first = 0
+        f = 0
+        read = fr.readlines()
+        for i, line in enumerate(read):
+            # For inline markdown
+            if "**" in line:
+                line = funcInline(line, "**")
+            if "__" in line:
+                line = funcInline(line, "__")
+            if "[[" in line and "]]" in line:
+                line = md5Markdown(line)
+            if "((" in line and "))" in line:
+                line = funcForCases(line)
+
+            # split by spaces
+            lineSplit = line.split(' ')
+            if lineSplit[0] in hash_dict:
+                # Headings
+                if lineSplit[0].startswith('#'):
+                    funcHeadings(lineSplit[0])
+                # Lists
+                elif lineSplit[0].startswith("-") or lineSplit[0].startswith("*"):
+                    tag = hash_dict[lineSplit[0]]
+                    #if its the first item list
+                    if not first:
+                        toWrite = "<{}>\n".format(tag)
+                        fw.write(toWrite)
+                        first = lineSplit[0]
+                    # do every time for '-' or '*'
+                    toWrite = line.replace("{} ".format(lineSplit[0]), "<li>")
+                    toWrite = toWrite[:-1] + ("</li>\n")
+                    fw.write(toWrite)
+                    # if its the last item list
+                    if i is len(read) - 1 or not read[i + 1].startswith("{} ".format(first)):
+                        toWrite = "</{}>\n".format(tag)
+                        fw.write(toWrite)
+                        first = 0
+            else:
+                # paragraphs
+                if line[0] != "\n":
+                    #first paragraph
+                    if not f:
+                        fw.write("<p>\n")
+                        f = 1
+                    fw.write(line)
+                    # if next line is part of the paragraph
+                    if i != len(read) - 1 and read[i + 1][0] != "\n" and read[i + 1][0] not in hash_dict:
+                        fw.write("<br/>\n")
+                    else:
+                        fw.write("</p>\n")
+                        f = 0
+        exit(0)
